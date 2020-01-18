90960c5604f298a73e982ba5a2d7ecce65c95a40
Supertao <taochao@huawei.com>
2020-01-18 21:47:05 +0800

    18、request 和 response显示出现bug

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 #echo "# Burp" >> README.md
 git init
 #git add README.md
-git commit -m '17、json fuzz payload 生成' -a
+git commit -m '18、request 和 response显示出现bug' -a
 git remote add origin git@github.com:Supertao/Burp.git
 git push -u origin master
